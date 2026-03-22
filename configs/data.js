3d630015bfb95496d4b140a88b42997877430393
Nahuel Tolosa <jntolosa98@gmial.com>
2026-03-22 03:47:16 -0300

    Se agregan nuevas fotos para cada categoria y se editan los mensajes

--- configs/data.js
+++ configs/data.js
@@ -23,13 +23,13 @@ const messages = {
                 desc: "¡Nuestra leyenda de 70 cm! Una experiencia gigante para compartir, con lluvia de papas pay y tus aderezos favoritos." 
             },
             { 
-                img: "img/tio_pancho.png", 
+                img: "img/tio_pancho_pizza.png", 
                 name: "Tío Pancho a la Pizza", 
                 price: "$17.000", 
                 desc: "70 cm de puro sabor gratinados con abundante muzzarella, salsa de tomate artesanal y un toque de orégano." 
             },
             { 
-                img: "img/tio_pancho.png", 
+                img: "img/tio_pancho_bondiola.png", 
                 name: "Tío Pancho a la Bondiola", 
                 price: "$17.000", 
                 desc: "70 cm de placer con bondiola desmechada. ¡Una combinación irresistible!" 
@@ -37,19 +37,19 @@ const messages = {
         ],
         superPancho: [
             { 
-                img: "img/tio_pancho.png", 
+                img: "img/super_pancho.png", 
                 name: "Súper Pancho Clásico", 
                 price: "$4.000", 
                 desc: "El Súper pancho que ya conocés: salchicha premium con lluvia de papas y aderezos a elección." 
             },
             { 
-                img: "img/tio_pancho.png", 
+                img: "img/super_pancho_pizza.png", 
                 name: "Súper Pancho a la Pizza", 
                 price: "$6.000", 
                 desc: "Súper salchicha gratinada con muzzarella fundida, salsa de tomate y el toque justo de orégano." 
             },
             { 
-                img: "img/tio_pancho.png", 
+                img: "img/super_pancho_bondiola.png", 
                 name: "Súper Pancho a la Bondiola", 
                 price: "$6.000", 
                 desc: "Súper pancho coronado con una generosa porción de nuestra bondiola desmechada artesanal." 
@@ -57,7 +57,7 @@ const messages = {
         ],
         panchito: [
             { 
-                img: "img/tio_pancho.png", 
+                img: "img/panchito.png", 
                 name: "Panchito Clásico", 
                 price: "$2.500", 
                 desc: "El tamaño ideal para los más chicos o para darte ese último gusto. Tierno, clásico y con papas." 
@@ -66,7 +66,7 @@ const messages = {
         hamburguesas: [
             { 
                 img: "img/tio_hamburguesa.png", 
-                name: "Doble Cheddar", 
+                name: "Burger", 
                 price: "$8.000", 
                 desc: "Medallón de carne vacuna, una montaña de queso cheddar fundido, vegetales y aderezos." 
             }
