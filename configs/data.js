fd1ae7c6c12eade8d9aaebc6575cc2f4c9902b9d
Nahuel Tolosa <jntolosa98@gmial.com>
2026-03-22 03:18:44 -0300

    Correcion en navegacion y edision de mensajes

--- configs/data.js
+++ configs/data.js
@@ -5,7 +5,9 @@ const messages = {
     },
     // Categorías que aparecerán en el menú superior
     categories: [
-        { id: "panchos", name: "🌭 Panchos" },
+        { id: "tioPancho", name: "🌭 Tío Pancho" },
+        { id: "superPancho", name: "🌭 Súper Pancho" },
+        { id: "panchito", name: "🌭 Panchito" },
         { id: "hamburguesas", name: "🍔 Hamburguesas" },
         { id: "pizzas", name: "🍕 Pizzas" },
         { id: "bebidas", name: "🥤 Bebidas" }
@@ -13,32 +15,60 @@ const messages = {
 
     // Productos separados por categoría
     menu: {
-        panchos: [
+        tioPancho: [
             { 
                 img: "img/tio_pancho.png", 
-                name: "Tio Pancho", 
+                name: "Tío Pancho Clásico", 
                 price: "$15.000", 
-                desc: "El mas grande de la ciudad! Pancho de 70 centimetros con aderezos y papas." 
+                desc: "¡Nuestra leyenda de 70 cm! Una experiencia gigante para compartir, con lluvia de papas pay y tus aderezos favoritos." 
             },
             { 
                 img: "img/tio_pancho.png", 
-                name: "Tio Pancho a la Pizza", 
+                name: "Tío Pancho a la Pizza", 
                 price: "$17.000", 
-                desc: "Pancho de 70 centimetros con salsa de tomate, muzzarella y oregano." 
+                desc: "70 cm de puro sabor gratinados con abundante muzzarella, salsa de tomate artesanal y un toque de orégano." 
             },
             { 
                 img: "img/tio_pancho.png", 
-                name: "Tio Pancho a la Bondiola", 
+                name: "Tío Pancho a la Bondiola", 
                 price: "$17.000", 
-                desc: "Pancho de 70 centimetros con bondiola desmechada." 
+                desc: "70 cm de placer con bondiola desmechada. ¡Una combinación irresistible!" 
             },
+        ],
+        superPancho: [
+            { 
+                img: "img/tio_pancho.png", 
+                name: "Súper Pancho Clásico", 
+                price: "$4.000", 
+                desc: "El Súper pancho que ya conocés: salchicha premium con lluvia de papas y aderezos a elección." 
+            },
+            { 
+                img: "img/tio_pancho.png", 
+                name: "Súper Pancho a la Pizza", 
+                price: "$6.000", 
+                desc: "Súper salchicha gratinada con muzzarella fundida, salsa de tomate y el toque justo de orégano." 
+            },
+            { 
+                img: "img/tio_pancho.png", 
+                name: "Súper Pancho a la Bondiola", 
+                price: "$6.000", 
+                desc: "Súper pancho coronado con una generosa porción de nuestra bondiola desmechada artesanal." 
+            },
+        ],
+        panchito: [
+            { 
+                img: "img/tio_pancho.png", 
+                name: "Panchito Clásico", 
+                price: "$2.500", 
+                desc: "El tamaño ideal para los más chicos o para darte ese último gusto. Tierno, clásico y con papas." 
+            }
         ],
         hamburguesas: [
             { 
                 img: "img/tio_hamburguesa.png", 
                 name: "Doble Cheddar", 
                 price: "$8.000", 
-                desc: "Medallón de carne, lechuga, tomate, huevo y aderezos a eleccion." 
+                desc: "Medallón de carne vacuna, una montaña de queso cheddar fundido, vegetales y aderezos." 
             }
         ],
         pizzas: [
@@ -46,15 +76,15 @@ const messages = {
                 img: "img/tio_pizza.png", 
                 name: "Muzzarella", 
                 price: "$10.000", 
-                desc: "Pizza a la piedra con salsa de tomate, queso muzzarella y orégano." 
+                desc: "Clásica pizza a la piedra, con muzzarella y orégano." 
             }
         ],
         bebidas: [
             { 
                 img: "img/tio_bebida.png", 
                 name: "Línea Coca-Cola 500ml", 
-                price: "$1200", 
-                desc: "Consultar disponibilidad." 
+                price: "$1.200", 
+                desc: "Variedad de sabores. Consultar disponibilidad al momento del pedido." 
             }
         ]
     },
--- js/navegation.js
+++ js/navegation.js
@@ -1,5 +1,13 @@
 // Armar navegación
 const navContainer = document.getElementById('nav-container');
+
+const arrowLeft = document.createElement('div');
+arrowLeft.className = 'nav-arrow';
+arrowLeft.innerHTML = `
+    <span> < </span>
+`
+navContainer.appendChild(arrowLeft);
+
 messages.categories.forEach((category, index) => {
     const btn = document.createElement('a');
     btn.href = `#${category.id}`;
@@ -12,3 +20,10 @@ messages.categories.forEach((category, index) => {
     });
     navContainer.appendChild(btn);
 });
+
+const arrowRight = document.createElement('div');
+arrowRight.className = 'nav-arrow';
+arrowRight.innerHTML = `
+    <span> > </span>
+`
+navContainer.appendChild(arrowRight);
